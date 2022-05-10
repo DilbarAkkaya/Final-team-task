89de941d2d7a1b36a341b61cbf838e699ea75bb8
evitla <s.dinmuhamed@gmail.com>
2022-05-10 12:52:32 +0600

    refactor: show user name in main page if user is authorized

--- project-management-app/src/pages/Main/Main.tsx
+++ project-management-app/src/pages/Main/Main.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { TStore } from '../../store';
 
 function Main() {
-  return <h2>Main page</h2>;
+  const { user } = useSelector((store: TStore) => store.userReducer);
+
+  return (
+    <>
+      <h2>Main page</h2>
+      {user && <h3>Hello, {user.name}</h3>}
+    </>
+  );
 }
 
 export default Main;
