afc4e4983b2193803292d22ba1c60da49b9fa023
evitla <s.dinmuhamed@gmail.com>
2022-05-11 12:53:02 +0600

    refactor: add users url, query key for users

--- project-management-app/src/constants/api.ts
+++ project-management-app/src/constants/api.ts
@@ -4,4 +4,9 @@ export const URLS = {
   base: BASE_URL,
   signup: `${BASE_URL}/signup`,
   signin: `${BASE_URL}/signin`,
+  users: `${BASE_URL}/users`,
+};
+
+export const QUERY_KEYS = {
+  users: 'users',
 };
